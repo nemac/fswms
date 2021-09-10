d3ec0c09ca3a1a68af2a7f486cdaf2d921eed97e
Matthew Geiger <mgeiger@unca.edu>
2021-09-10 14:41:39 +0000

    Use https proxy on AWS

--- msconfig/usdm.py
+++ msconfig/usdm.py
@@ -23,7 +23,7 @@ class USDM():
 
   def render_url(self, date):
     datestring = date.strftime('%Y%m%d')
-    url = "http://ndmc-001.unl.edu:8080/cgi-bin/mapserv.exe?map=/ms4w/apps/usdm/service/usdm_{datestring}_wms.map&amp;TRANSPARENT=true".format(datestring=datestring)
+    url = "{url}/cgi-bin/mapserv.exe?map=/ms4w/apps/usdm/service/usdm_{datestring}_wms.map&amp;TRANSPARENT=true".format(datestring=datestring, url=USDM_PROXY_API)
     return url
 
   def render_layer(self, lid, drt_url, name, title):
