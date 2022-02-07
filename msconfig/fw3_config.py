f2d48b7a9cf02f47d5ae01fd6135383798c0d5b3
Jeff Bliss <jeff.a.bliss@gmail.com>
2022-02-07 20:02:06 +0000

    update for 2022 archive layers

--- msconfig/fw3_config.py
+++ msconfig/fw3_config.py
@@ -1,3 +1,17 @@
+'''
+Main config for describing FW3 layer lists in FCAV
+
+ - Top-level keys 'normal' and 'muted' differentiate normal vs muted meta-types
+   - Muted products are the square root of the normal product at each pixel
+ - The next level down describes the product types for each meta-type
+   - Keys are the names of directories in FW3_DATA_DIR (see ../var/Config.py)
+ - Each object describing a product type has the following keys:
+   - title: The HTML-escaped title of the layer list in the FCAV
+   - info: A description of the product type, displayed under the title
+   - order: The order the layer list appears in the FCAV (includes
+   - break: Indicates that an extra linebreak should follow this layer list in the FCAV
+     - If not present will default to True (see fw3.py)
+'''
 
 FW3_PRODUCT_TYPES = {
   'normal': {
